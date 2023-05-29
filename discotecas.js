9397eea79cd14f5486b59cc172be803060f8f118
Amper23 <133078833+Amper23@users.noreply.github.com>
2023-05-28 23:15:13 -0500

    Add files via upload

---
+++ discotecas.js
@@ -0,0 +1,26 @@
+var discotecas_geoJson = {
+    "type": "FeatureCollection",
+    "name": "discotecas",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+        { "type": "Feature", "properties": { "id": 1, "etiqueta": "Discotecas parque de la caña" }, "geometry": { "type": "Point", "coordinates": [ -76.50675125067086, 3.453359247063134 ] } },
+        { "type": "Feature", "properties": { "id": 2, "etiqueta": "Centro comercial santiago plaza" }, "geometry": { "type": "Point", "coordinates": [ -76.500935789818939, 3.444403511765244 ] } },
+        { "type": "Feature", "properties": { "id": 3, "etiqueta": "Autopista simon bolivar" }, "geometry": { "type": "Point", "coordinates": [ -76.491113130414519, 3.445117886994657 ] } },
+        { "type": "Feature", "properties": { "id": 4, "etiqueta": "Micelania" }, "geometry": { "type": "Point", "coordinates": [ -76.506650791654238, 3.448957653852747 ] } },
+        { "type": "Feature", "properties": { "id": 5, "etiqueta": "Parque Industrial " }, "geometry": { "type": "Point", "coordinates": [ -76.511800246432912, 3.449999451062306 ] } },
+        { "type": "Feature", "properties": { "id": 6, "etiqueta": "Kiss me" }, "geometry": { "type": "Point", "coordinates": [ -76.516056732174832, 3.444284449227009 ] } },
+        { "type": "Feature", "properties": { "id": 7, "etiqueta": "Sena de la construción" }, "geometry": { "type": "Point", "coordinates": [ -76.507156807441731, 3.43990890094686 ] } },
+        { "type": "Feature", "properties": { "id": 8, "etiqueta": "MERECÚ" }, "geometry": { "type": "Point", "coordinates": [ -76.501841781320806, 3.44040561497356 ] } },
+        { "type": "Feature", "properties": { "id": 9, "etiqueta": "MAKEY CLUB DISCOTECA" }, "geometry": { "type": "Point", "coordinates": [ -76.501871546955371, 3.441990635013818 ] } },
+        { "type": "Feature", "properties": { "id": 10,"etiqueta": "Video Bar" }, "geometry": { "type": "Point", "coordinates": [ -76.511281208180293, 3.439922853588059 ] } },
+        { "type": "Feature", "properties": { "id": 11,"etiqueta": "Galeria de la floresta" }, "geometry": { "type": "Point", "coordinates": [ -76.512631823848409, 3.440964650797618 ] } }
+    ]
+};
+
+
+
